9a2ad21128deb4eaa631460632fabd46d1d6b788
RedShakespeare <silencess_m@qq.com>
2025-12-03 22:01:17 +0800

    [feat] 赞助按钮展示小蛋糕

---
+++ scripts/inject-reward-css.js
@@ -0,0 +1,16 @@
+'use strict';
+
+hexo.extend.injector.register('head_end', `
+<style>
+is-j.φcm .φct > a.φcs{
+  position: relative;
+  font-size: 0 !important;
+  line-height: 1 !important;
+}
+is-j.φcm .φct > a.φcs::before{
+  content: "🧁" !important;
+  font-size: 14px;
+  line-height: 1.2;
+}
+</style>
+`);
